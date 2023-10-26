f24949ae0e58e166e1ad4ad8ff4b16b70bc4c99d
Daniel Napierski <hidden>
2023-10-26 10:41:41 -0400

    reading 3 wallets

--- dang_genius/geminiexchange.py
+++ dang_genius/geminiexchange.py
@@ -1,12 +1,120 @@
+import base64
+import datetime
+import hashlib
+import hmac
+import json
+from typing import List
+
+import requests
+import time
 from dang_genius.exchange import Exchange
 
 
 class GeminiExchange(Exchange):
     def __init__(self, key: str, secret: str, btc_amount: float):
         super().__init__(key, secret, btc_amount)
+        self.api_url = 'https://api.gemini.com'
+        self.order_endpoint = '/v1/order/new'
+        self.balances_endpoint = '/v1/balances'
+
+    def get_nonce(self) -> str:
+        return str(int(9999999999999 + time.mktime(datetime.datetime.now().timetuple()) * 1000))
+
+    def get_balances(self):
+        account: List[str] = ["primary"]
+        payload = {}
+        request_url = self.api_url + self.balances_endpoint
+        payload["request"] = self.balances_endpoint
+        payload["nonce"] = self.get_nonce()
+        encoded_payload = json.dumps(payload).encode("utf-8")
+        b64 = base64.b64encode(encoded_payload)
+        signature = hmac.new(
+            self.secret.encode("utf-8"), b64, hashlib.sha384
+        ).hexdigest()
+
+        request_headers = {
+            "Content-Type": "text/plain",
+            "Content-Length": "0",
+            "X-GEMINI-APIKEY": self.key,
+            "X-GEMINI-PAYLOAD": b64,
+            "X-GEMINI-SIGNATURE": signature,
+            "Cache-Control": "no-cache",
+        }
+
+        request = requests.post(
+            request_url, data=None, headers=request_headers
+        )
+        data = request.json()
+        btc = 0.0
+        usd = 0.0
+        for d in data:
+            if d.get('currency') == 'USD':
+                usd = float(d.get('available'))
+            if d.get('currency') == 'BTC':
+                btc = float(d.get('available'))
+
+        return {'BTC': btc, 'USD': usd}
 
     def buy_btc(self):
         print(f'BUY {self.btc_amount:.5f} BTC GEMINI')
+        self.trade('buy')
+        print(f'COMPLETED BUY {self.btc_amount:.5f} BTC GEMINI')
 
     def sell_btc(self):
         print(f'SELL {self.btc_amount:.5f} BTC GEMINI')
+        self.trade('sell')
+        print(f'COMPLETED SELL {self.btc_amount:.5f} BTC GEMINI')
+
+    def trade(self, side: str) -> object:
+        payload = {
+            "request": self.order_endpoint,
+            "nonce": self.get_nonce(),
+            "symbol": "btcusd",
+            "amount": self.btc_amount,
+            "side": side,
+            "type": "exchange limit",
+            # TODO: fix market?
+        }
+        encoded_payload = json.dumps(payload).encode()
+        b64 = base64.b64encode(encoded_payload)
+        signature = hmac.new(self.secret.encode(), b64, hashlib.sha384).hexdigest()
+        request_headers = {'Content-Type': "text/plain",
+                           'Content-Length': "0",
+                           'X-GEMINI-APIKEY': self.key,
+                           'X-GEMINI-PAYLOAD': b64,
+                           'X-GEMINI-SIGNATURE': signature,
+                           'Cache-Control': "no-cache"}
+
+        try:
+            new_order = requests.post(self.api_url + self.order_endpoint,
+                                      data=None,
+                                      headers=request_headers).json()
+            if new_order.get('result') == 'error':
+                print(f'GEMINI ERROR: {new_order}')
+            else:
+                print(f'GEMINI SUCCESS: {new_order}')
+
+        except Exception as e:
+            print(f'GEMINI EXCEPTION: {e}')
+
+# if not payload:
+
+# check_status = new_order['order_id']
+# notes below
+# import os
+# from dotenv import load_dotenv
+# from gemini_api.authentication import Authentication
+# from gemini_api.endpoints.fund_management import FundManagement
+#
+# load_dotenv()
+#
+# auth = Authentication(
+#    public_key=GE_API_KEY, private_key=GE_API_SECRET
+# )
+# if __name__ == "__main__":
+#   x = FundManagement.get_notional_balances(
+#      auth=auth, currency='USD'
+#    )
+#    y = x[0]
+#    print(getattr(y, 'amount'))
+#    print(getattr(y, 'currency'))
--- dang_genius/krakenexchange.py
+++ dang_genius/krakenexchange.py
@@ -1,60 +1,96 @@
+import base64
+import hashlib
+import json
+import hmac
+import time
+import urllib.parse
+import requests
 from dang_genius.exchange import Exchange
 
 
 class KrakenExchange(Exchange):
+
     def __init__(self, key: str, secret: str, btc_amount: float):
         super().__init__(key, secret, btc_amount)
+        self.api_url = "https://api.kraken.com"
+
+    def get_kraken_signature(self, urlpath, data, secret):
+        postdata = urllib.parse.urlencode(data)
+        encoded = (str(data['nonce']) + postdata).encode()
+        message = urlpath.encode() + hashlib.sha256(encoded).digest()
+        mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
+        sigdigest = base64.b64encode(mac.digest())
+        return sigdigest.decode()
+
+    def kraken_request(self, uri_path, data, api_key, api_sec):
+        headers = {'API-Key': api_key, 'API-Sign': self.get_kraken_signature(uri_path, data, api_sec)}
+        # get_kraken_signature() as defined in the 'Authentication' section
+        req = requests.post((self.api_url + uri_path), headers=headers, data=data)
+        return req
 
     def buy_btc(self):
-        print(f'BUY {self.btc_amount:.5f} BTC KRAKEN')
+        print(f'BUY {self.btc_amount:.5f} BTC KRAKEN ...')
+        # Construct the request and print the result
+        resp = self.kraken_request('/0/private/AddOrder', {
+            "nonce": str(int(1000 * time.time())),
+            "ordertype": "market",
+            "type": "buy",
+            "volume": self.btc_amount,
+            "pair": "XBTUSD",
+        }, self.key, self.secret)
+        print(f'STARTED BUY {self.btc_amount:.5f} BTC KRAKEN')
+        text_resp = getattr(resp, 'text')
+        j = json.loads(text_resp)
+        error = j.get('error')
+        if error:
+            print(f'KRAKEN ERROR: {error}')
+        else:
+            print(f'KRAKEN SUCCESS: {j}')
+        print('</KRAKEN>')
 
     def sell_btc(self):
         print(f'SELL {self.btc_amount:.5f} BTC KRAKEN DONT WE HAVE BTCCCCCCC')
 
-    def buyBitcoin(self, amount: float) -> dict:
-        print(f'{amount:.5f}')
+# TODO: Delete NOTES
+#    data = {
+#        "nonce": "1616492376594",
+#        "ordertype": "limit",
+#        "pair": "XBTUSD",
+#        "price": 37500,
+#        "type": "buy",
+#        "volume": 1.25
+#    }
+#    signature = get_kraken_signature("/0/private/AddOrder", data, self.secret)
+#    print("API-Sign: {}".format(signature))
+#    def buyBitcoin(self, amount: float) -> dict:
+#        print(f'{amount:.5f}')
 #        KR_API_KEY = os.environ.get('KR-API-KEY')
 #        KR_API_SECRET = os.environ.get('KR-API-SECRET')
-
 #        k = krakenex.API(KR_API_KEY, KR_API_SECRET)
 #        b = k.query_private('Balance')['result']
 #        btc_b = float(b.get('XXBT'))
-#         # TODO: kraken USD?
+# TODO: kraken USD?
 #        usd_b = float(-1.0)
 #        return {'BTC': btc_b, 'USD': usd_b}
-
-
-
-        return dict(MSG="Failed")
-
-
-
-#import time
-#import os
-#import requests
-
+#        return dict(MSG="Failed")
 # Read Kraken API key and secret stored in environment variables
-#api_url = "https://api.kraken.com"
-#api_key = os.environ['API_KEY_KRAKEN']
-#api_sec = os.environ['API_SEC_KRAKEN']
-
+# api_key = os.environ['API_KEY_KRAKEN']
+# api_sec = os.environ['API_SEC_KRAKEN']
 # Attaches auth headers and returns results of a POST request
-#def kraken_request(uri_path, data, api_key, api_sec):
+# def kraken_request(uri_path, data, api_key, api_sec):
 #    headers = {}
 #    headers['API-Key'] = api_key
-    # get_kraken_signature() as defined in the 'Authentication' section
+# get_kraken_signature() as defined in the 'Authentication' section
 #    headers['API-Sign'] = get_kraken_signature(uri_path, data, api_sec)
 #    req = requests.post((api_url + uri_path), headers=headers, data=data)
 #    return req
-
 # Construct the request and print the result
-#resp = kraken_request('/0/private/AddOrder', {
+# resp = kraken_request('/0/private/AddOrder', {
 #    "nonce": str(int(1000*time.time())),
 #    "ordertype": "limit",
 #    "type": "buy",
 #    "volume": 1.25,
 #    "pair": "XBTUSD",
 #    "price": 27500
-#}, api_key, api_sec)
-
-#print(resp.json())
+# }, api_key, api_sec)
+# print(resp.json())
